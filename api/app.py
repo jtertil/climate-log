eb2ee4e67f31f99dbd35e7533f933e9ac3effe96
jtertil <jtertil@gmail.com>
2019-12-03 18:32:01 +0100

    api logic test

--- api/app.py
+++ api/app.py
@@ -1,8 +1,29 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
+
+fake_data = {}
+fake_auth = {'sensor1': 'QYZ'}
 
 
 @app.route('/')
 def index():
     return 'flask run ok'
 
+
+@app.route('/data/<string:sensor>')
+def data(sensor):
+    try:
+        return fake_data[sensor]
+    except KeyError:
+        return f'no such sensor: {sensor}'
+
+
+@app.route('/log/', methods=['POST'])
+def log():
+    req_data = request.get_json()
+    sensor = req_data['sensor']
+
+    if req_data['key'] == fake_auth[sensor]:
+        fake_data[sensor] = (req_data['values'])
+
+    return '1'
