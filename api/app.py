f55bab804b6d5358ee9e5c03dd78ba61487916e8
jtertil <jtertil@gmail.com>
2019-12-03 23:35:29 +0100

    database communication prototype

--- api/app.py
+++ api/app.py
@@ -1,8 +1,20 @@
+import os
+
+
 from flask import Flask, request
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
 app = Flask(__name__)
 
-fake_data = {}
-fake_auth = {'sensor1': 'QYZ'}
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# Setup database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
 
 
 @app.route('/')
@@ -10,20 +22,53 @@ def index():
     return 'flask run ok'
 
 
-@app.route('/data/<string:sensor>')
-def data(sensor):
+@app.route('/data/<string:name>')
+def data(name):
+
+
     try:
-        return fake_data[sensor]
-    except KeyError:
-        return f'no such sensor: {sensor}'
+        q = db.execute(
+            'SELECT * FROM public.' + name + ' '
+            'ORDER BY time DESC'
+        ).first()
+
+        d = {'last read': q[1], 'total reads': q[0],
+             'values': {'temperature': float(q[2]), 'humidity': float(q[3])}}
+
+        return d
+
+    except Exception as e:
+        return str(e)
 
 
 @app.route('/log/', methods=['POST'])
 def log():
     req_data = request.get_json()
-    sensor = req_data['sensor']
+    name = req_data['name']
 
-    if req_data['key'] == fake_auth[sensor]:
-        fake_data[sensor] = (req_data['values'])
+    sensor_key = db.execute(
+        'SELECT key FROM public.sensor '
+        'WHERE name = :name',
+        {'name': name}
+    ).first()
 
-    return '1'
+    if not sensor_key:
+        return f'no such sensor: {name}'
+
+    elif req_data['key'] != sensor_key[0]:
+        return 'API key no valid'
+
+    else:
+        temperature = req_data['values']['temperature']
+        humidity = req_data['values']['humidity']
+
+        db.execute(
+            'INSERT INTO public.' + name + ' (temperature, humidity) '
+            'VALUES (:temperature, :humidity) ',
+            {'temperature': temperature,
+             'humidity': humidity
+             })
+
+        db.commit()
+        return 'update ok'
+
